1f6c6a775c6ebfa1a09f744065d3674054059158
Abhyuday Pandey <34156497+abhyuday07@users.noreply.github.com>
2018-12-01 12:55:13 +0530

    Add files via upload

---
+++ tmpsee.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned int uint;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+
+#define fst first
+#define snd second
+#define mp make_pair
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define fill(a) memset(a,0,sizeof(a))
+#define uni(c) c.resize(distance(c.begin(),unique(all(c)))
+
+#define rep(i,n) for(ll i=0,_n=(n); i<_n; i++)
+#define rep1(i,n) for(ll i=1, _n=(n); i<=_n; i++)
+#define repA(i, a, n) for(ll i=a,_n=(n); i<=_n; i++)
+#define repD(i, a, n) for(ll i=a,_n=(n); i>=_n; --i)
+#define cout(d) cout << fixed << setprecision(d)
+
+const ld eps=1e-12, pi=acosl(-1);
+const ll inf=1e16, MOD=1e9+7;
+const int N=2e6+10;
